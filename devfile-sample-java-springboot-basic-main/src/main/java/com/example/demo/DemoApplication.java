77f317649df2d9747b186de8953310b1bc1c58b1
ulisesbf <67929054+ulisesbf@users.noreply.github.com>
2022-04-13 18:51:19 -0500

    Add files via upload

--- devfile-sample-java-springboot-basic-main/src/main/java/com/example/demo/DemoApplication.java
+++ devfile-sample-java-springboot-basic-main/src/main/java/com/example/demo/DemoApplication.java
@@ -2,29 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.lettuce.core.*;
 import io.lettuce.core.api.*;
 import io.lettuce.core.api.sync.*;
+import redis.clients.jedis.Jedis;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-     	 RedisClient redisClient = RedisClient.create(RedisURI.create("127.0.0.1:", 6379));
-       	StatefulRedisConnection<String, String> connection = redisClient.connect();
-       	RedisCommands<String, String> syncCommands = connection.sync();
-
-       	syncCommands.set("key", "Hello, Redis!");
-
-       	connection.close();
-       	redisClient.shutdown();
-     	
+    	  Jedis jedis = new Jedis("127.0.0.1", 6379);
+		    jedis.auth("Armani16");
+		    System.out.println("Connected to Redis");
+		    jedis.set("foo", "bar");
+		    String value = jedis.get("foo");
+		    System.out.println(value);
     
-        return "Hello World!";
+        return "Hello World! "+value;
     }
 
     public static void main(String[] args) {
